9042c165933444fe4b0e834ccc6ee98cc7b455d8
Лилиана <lilya_gazina@mail.ru>
2023-07-20 12:54:39 +0300

    hw 4.4 готова

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
@@ -18,35 +20,46 @@ public class StudentService {
         //this.avatarRepository = avatarRepository;
     }
 
+   Logger logger = LoggerFactory.getLogger(StudentService.class);
+
+
     public Student createStudent(Student student) {
+        logger.info("Was invoked method for create student");
         return studentRepository.save(student);
     }
 
     public Student getStudentById(Long studentId) {
+        logger.info("Was invoked method get with studentId = {}", studentId);
         return studentRepository.findById(studentId).get();
     }
 
     public Student updateStudent(Long studentId, Student student) {
+        logger.info("Was invoked method update with studentId = {}", studentId);
         return studentRepository.save(student);
     }
 
     public void deleteStudent(Long studentId) {
+        logger.info("Was invoked method delete with studentId = {}", studentId);
         studentRepository.deleteById(studentId);
     }
 
     public Collection<Student> getAllStudent() {
+        logger.info("Was invoked method getAllStudent ");
         return studentRepository.findAll();
     }
 
     public Collection<Student> findByAge(int age) {
+        logger.info("Was invoked method findByAge");
         return studentRepository.findByAge(age);
     }
 
     public Collection<Student> findByAgeBetween(int min, int max) {
+        logger.info("Was invoked method findByAgeBetween");
         return studentRepository.findByAgeBetween(min, max);
     }
 
     public Faculty getFacultyStudent(Long id) {
+        logger.info("Was invoked method getFacultyStudent with id = {}", id);
         Student student = studentRepository.findById(id).orElse(null);
         if (student == null) {
             return null;
@@ -55,15 +68,18 @@ public class StudentService {
 
     }
     public Long getTotalNumberStudents(){
+        logger.info("Was invoked method getTotalNumberStudents");
        return studentRepository.getTotalNumberStudents();
 
     }
 
     public Long getAverageAgeStudents(){
+        logger.info("Was invoked method getAverageAgeStudents");
        return studentRepository.getAverageAgeStudents();
     }
     public Collection<Student> getLastFiveStudents(){
-       return studentRepository.getLastFiveStudents();
+        logger.info("Was invoked method getLastFiveStudents");
+        return studentRepository.getLastFiveStudents();
     }
 
 }
